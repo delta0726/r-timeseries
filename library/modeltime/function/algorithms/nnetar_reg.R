3332b1598cdda1b4436fb9b74fab8ed22b6b5dd4
Keido Sumi <delta0726@nta.pial.jp>
2021-08-07 07:33:58 +0900

    Update {modeltime}

--- library/modeltime/function/algorithms/nnetar_reg.R
+++ library/modeltime/function/algorithms/nnetar_reg.R
@@ -1,20 +1,70 @@
-# Title     :
-# Objective : TODO
-# Created by: Owner
-# Created on: 2020/8/23
-# URL       :
+# ***************************************************************************************
+# Library   : modeltime
+# Function  : nnetar_reg
+# Created on: 2021/8/7
+# URL       : https://business-science.github.io/modeltime/reference/nnetar_reg.html
+# ***************************************************************************************
 
 
-
-# ＜ポイント＞
-
-
-
+# ＜概要＞
+# - ニューラルネットワークによるカオス時系列の予測
 
 
 # ＜構文＞
+# nnetar_reg(
+#   mode = "regression",
+#   seasonal_period = NULL,
+#   non_seasonal_ar = NULL,
+#   seasonal_ar = NULL,
+#   hidden_units = NULL,
+#   num_networks = NULL,
+#   penalty = NULL,
+#   epochs = NULL
+# )
 
 
+# ＜引数＞
 
 
+# ＜使用例＞
+# 0 準備
+# 1 NNETAR
 
+
+# 0 準備 --------------------------------------------------------------------------------
+
+# ライブラリ
+library(dplyr)
+library(parsnip)
+library(rsample)
+library(timetk)
+library(modeltime)
+
+
+# データ準備
+# --- 1系列を抽出
+m750 <- m4_monthly %>% filter(id == "M750")
+
+# データ確認
+m750 %>% print()
+
+# データ分割
+splits <- m750 %>% initial_time_split(prop = 0.8)
+
+
+# 1 NNETAR ------------------------------------------------------------------------------
+
+# モデル構築
+model_spec <-
+  nnetar_reg() %>%
+    set_engine("nnetar")
+
+# 学習
+set.seed(123)
+model_fit <-
+  model_spec %>%
+    fit(log(value) ~ date, data = training(splits))
+
+# 確認
+model_spec %>% print()
+model_fit %>% print()
--- library/modeltime/function/algorithms/prophet_boost.R
+++ library/modeltime/function/algorithms/prophet_boost.R
@@ -1,20 +1,90 @@
-# Title     :
-# Objective : TODO
-# Created by: Owner
-# Created on: 2020/8/23
-# URL       :
+# ***************************************************************************************
+# Library   : modeltime
+# Function  : prophet_boost
+# Created on: 2021/8/7
+# URL       : https://business-science.github.io/modeltime/reference/prophet_boost.html
+# ***************************************************************************************
 
 
-
-# ＜ポイント＞
-
-
-
+# ＜概要＞
+# - プロフェットブースティング
 
 
 # ＜構文＞
+# prophet_boost(
+#   mode = "regression",
+#   growth = NULL,
+#   changepoint_num = NULL,
+#   changepoint_range = NULL,
+#   seasonality_yearly = NULL,
+#   seasonality_weekly = NULL,
+#   seasonality_daily = NULL,
+#   season = NULL,
+#   prior_scale_changepoints = NULL,
+#   prior_scale_seasonality = NULL,
+#   prior_scale_holidays = NULL,
+#   logistic_cap = NULL,
+#   logistic_floor = NULL,
+#   mtry = NULL,
+#   trees = NULL,
+#   min_n = NULL,
+#   tree_depth = NULL,
+#   learn_rate = NULL,
+#   loss_reduction = NULL,
+#   sample_size = NULL,
+#   stop_iter = NULL
+# )
 
 
+# ＜引数＞
 
 
+# ＜使用例＞
+# 0 準備
+# 1 モデル構築
+# 2 モデル学習
 
+
+# 0 準備 --------------------------------------------------------------------------------
+
+# ライブラリ
+library(dplyr)
+library(parsnip)
+library(rsample)
+library(timetk)
+library(modeltime)
+library(lubridate)
+
+
+# データ準備
+# --- 1系列を抽出
+m750 <- m4_monthly %>% filter(id == "M750")
+
+# データ確認
+m750 %>% print()
+
+# データ分割
+splits <- m750 %>% initial_time_split(prop = 0.8)
+
+
+# 1 モデル構築 -------------------------------------------------------------------------
+
+# モデル構築
+model_spec <-
+  prophet_boost(learn_rate = 0.1) %>%
+    set_engine("prophet_xgboost")
+
+# 確認
+model_spec %>% print()
+
+
+# 2 モデル学習 --------------------------------------------------------------------------
+
+# 学習
+model_fit <-
+  model_spec %>%
+      fit(log(value) ~ date + as.numeric(date) + month(date, label = TRUE),
+          data = training(splits))
+
+# 確認
+model_fit %>% print()
