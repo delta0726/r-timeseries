f2306904f8aa0900348b284e7f48a5bc26358b14
Keido Sumi <delta0726@nta.pial.jp>
2021-08-15 07:23:06 +0900

    Update {timetk}

--- library/timetk/function/recipe/step_ts_impute.R
+++ library/timetk/function/recipe/step_ts_impute.R
@@ -1,20 +1,112 @@
-# Title     :
-# Objective : TODO
-# Created by: Owner
-# Created on: 2020/8/23
-# URL       :
+# ***************************************************************************************
+# Library   : timetk
+# Function  : step_ts_impute
+# Created on: 2021/8/15
+# URL       : https://business-science.github.io/timetk/reference/step_ts_impute.html
+# ***************************************************************************************
 
 
-
-# ＜ポイント＞
-
-
-
+# ＜概要＞
+# - 欠損値を追加するレシピステップ
+#   --- recipes::ts_impute_vec()をレシピ化したもの
 
 
 # ＜構文＞
+# step_ts_impute(
+#   recipe,
+#   ...,
+#   period = 1,
+#   lambda = NULL,
+#   role = NA,
+#   trained = FALSE,
+#   lambdas_trained = NULL,
+#   skip = FALSE,
+#   id = rand_id("ts_impute")
+# )
 
 
+# ＜引数＞
+# - period : period = 1（季節性なし）
+#          : period > 1（季節性あり）
 
 
+# ＜目次＞
+# 0 準備
+# 1 レシピの作成
+# 2 レシピサマリーの確認
+# 3 データ比較
 
+
+# 0 準備 ---------------------------------------------------------------------
+
+# ライブラリ
+library(tidyverse)
+library(tidyquant)
+library(recipes)
+library(timetk)
+
+# データ準備
+# --- pad_by_time()でFiveDayをSevenDayに変更（NAを発生指せる）
+FANG_wide <-
+  FANG %>%
+    select(symbol, date, adjusted) %>%
+    pivot_wider(names_from = symbol, values_from = adjusted) %>%
+    pad_by_time(.date_var = date)
+
+# データ確認
+# --- 土日がNAとして表示されていることを確認
+FANG_wide %>% print()
+FANG_wide %>% glimpse()
+
+
+# 1 レシピの作成 -------------------------------------------------------------
+
+# レシピ定義
+# --- 指定した系列に欠損値補完を適用
+# --- 季節性調整が適用されている（period = 252）
+step_ts_impute <-
+  recipe(~ ., data = FANG_wide) %>%
+    step_ts_impute(FB, AMZN, NFLX, GOOG, period = 252, lambda = "auto")
+
+# 確認
+step_ts_impute %>% prep() %>% bake(new_data = FANG_wide)
+step_ts_impute %>% prep() %>% bake(new_data = FANG_wide) %>% glimpse()
+
+
+# 2 レシピサマリーの確認 ---------------------------------------------------------
+
+# サマリー
+# --- 指定系列を変換しているので同じ結果
+step_ts_impute %>% summary()
+step_ts_impute %>% prep() %>% summary()
+
+# 処理内容の表示
+# --- トップレベル
+# --- 詳細レベル（最適ラムダが表示される）
+step_ts_impute %>% prep() %>% tidy()
+step_ts_impute %>% prep() %>% tidy(1)
+
+
+# 3 データ比較 -----------------------------------------------------------------
+
+# レシピ適用前のデータ
+FANG_wide_bef <-
+  FANG_wide %>%
+    mutate(type = "before")
+
+# レシピ適用後のデータ
+FANG_wide_aft <-
+  step_ts_impute %>%
+    prep() %>%
+    bake(new_data = FANG_wide) %>%
+    mutate(type = "after")
+
+# プロット比較
+# --- 季節性
+FANG_wide_bef %>%
+  bind_rows(FANG_wide_aft) %>%
+  filter_by_time(.start_date = "start", .end_date = "2013-12-31") %>%
+  pivot_longer(-c("date", "type")) %>%
+  filter(name == "FB") %>%
+  ggplot(aes(x = date, y = value, color = type)) +
+  geom_line()
--- library/timetk/function/recipe/step_ts_pad.R
+++ library/timetk/function/recipe/step_ts_pad.R
@@ -1,20 +1,79 @@
-# Title     :
-# Objective : TODO
-# Created by: Owner
-# Created on: 2020/8/23
-# URL       :
+# ***************************************************************************************
+# Library   : timetk
+# Function  : step_ts_pad
+# Created on: 2021/8/15
+# URL       : https://business-science.github.io/timetk/reference/step_ts_pad.html
+# ***************************************************************************************
 
 
-
-# ＜ポイント＞
-
-
-
+# ＜概要＞
+# - 指定された時間間隔で欠損した日付を補完するレシピステップ
+#   --- recipes::pad_by_time()をレシピ化したもの
 
 
 # ＜構文＞
+# step_ts_pad(
+#   recipe,
+#   ...,
+#   by = "day",
+#   pad_value = NA,
+#   role = "predictor",
+#   trained = FALSE,
+#   columns = NULL,
+#   skip = FALSE,
+#   id = rand_id("ts_padding")
+# )
 
 
+# ＜目次＞
+# 0 準備
+# 1 レシピの作成
+# 2 レシピサマリーの確認
 
 
+# 0 準備 ---------------------------------------------------------------------
 
+# ライブラリ
+library(tidyverse)
+library(tidyquant)
+library(recipes)
+library(timetk)
+
+# データ準備
+FB_tbl <-
+  FANG %>%
+    filter(symbol == "FB") %>%
+    select(symbol, date, adjusted)
+
+# データ確認
+# --- 土日の日付が含まれないことを確認
+FB_tbl %>% print()
+FB_tbl %>% glimpse()
+
+
+# 1 レシピの作成 -------------------------------------------------------------
+
+# レシピ定義
+# --- 土日のレコードを追加
+recipe_ts_pad <-
+  recipe(~ ., data = FB_tbl) %>%
+    step_ts_pad(date, by = "day", pad_value = NA)
+
+# 確認
+# --- 土日のレコードを追加されている
+recipe_ts_pad %>% prep() %>% bake(new_data = FB_tbl)
+recipe_ts_pad %>% prep() %>% bake(new_data = FB_tbl) %>% glimpse()
+
+
+# 2 レシピサマリーの確認 ---------------------------------------------------------
+
+# サマリー
+# --- 指定系列を変換しているので同じ結果
+recipe_ts_pad %>% summary()
+recipe_ts_pad %>% prep() %>% summary()
+
+# 処理内容の表示
+# --- トップレベル
+# --- 詳細レベル（最適ラムダが表示される）
+recipe_ts_pad %>% prep() %>% tidy()
+recipe_ts_pad %>% prep() %>% tidy(1)
