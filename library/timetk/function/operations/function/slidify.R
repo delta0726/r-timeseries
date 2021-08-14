66090041ad486baba6d93b8a1964b61401c02620
Keido Sumi <delta0726@nta.pial.jp>
2021-08-14 09:09:11 +0900

    Update {timetk}

--- library/timetk/function/operations/function/slidify.R
+++ library/timetk/function/operations/function/slidify.R
@@ -1,15 +1,13 @@
-# Title     : slidify
-# Objective : TODO
-# Created by: Owner
-# Created on: 2020/8/23
+# ***************************************************************************************
+# Library   : timetk
+# Function  : slidify
+# Created on: 2021/8/14
 # URL       : https://business-science.github.io/timetk/reference/slidify.html
+# ***************************************************************************************
 
 
-
-# ＜ポイント＞
-# - ローリング計算のための関数を定義しる
-
-
+# ＜概要＞
+# - ローリング計算のための関数を定義する
 
 
 # ＜構文＞
@@ -27,22 +25,31 @@
 # - partial : 要素数が足りない場合も計算するか
 
 
+# ＜目次＞
+# 0 準備
+# 1 単純なローリング計算
+# 2 要素数が足りない箇所の出力
+# 3 複数のローリング系列を一度に追加
+# 4 グループごとにローリング計算
+# 5 複数引数を持つローリング計算
+# 6 関数定義/ラムダ式で定義
+# 7 ローリング回帰
 
 
-# 1.準備 -----------------------------------------------------
+# 0 準備 -----------------------------------------------------------------------
 
+# ライブラリ
 library(tidyverse)
 library(tidyquant)
 library(tidyr)
 library(timetk)
-
 
 # データ準備
 data(FANG)
 FB <- FANG %>% filter(symbol == "FB")
 
 
-# 2.単純なローリング計算 -----------------------------------------------------
+# 1 単純なローリング計算 -----------------------------------------------------
 
 # 関数定義
 # --- 移動平均
@@ -54,20 +61,19 @@ mean_roll_10 <- slidify(mean, .period = 10, .align = "right")
 FB %>%
   mutate(rolling_mean_5 = mean_roll_5(adjusted))
 
-
+# 列追加
+# --- 複数の移動平均
 FB %>%
   select(symbol, date, adjusted) %>%
   mutate(rolling_mean_5  = mean_roll_5(adjusted),
          rolling_mean_10 = mean_roll_10(adjusted))
 
 
-
-# 3.要素数が足りない箇所の出力 -----------------------------------------------------
+# 2 要素数が足りない箇所の出力 -----------------------------------------------------
 
 # 関数定義
 # --- 移動平均
 mean_roll_5_partial <- slidify(mean, .period = 5, .align = "right", .partial = TRUE)
-
 
 # 列追加
 # --- 移動平均
@@ -75,8 +81,7 @@ FB %>%
   mutate(rolling_mean_5 = mean_roll_5_partial(adjusted))
 
 
-
-# 4.複数のローリング系列を一度に追加 -----------------------------------------------------
+# 3 複数のローリング系列を一度に追加 -----------------------------------------------------
 
 FB %>%
   select(symbol, date, adjusted) %>%
@@ -84,10 +89,7 @@ FB %>%
                      .names = str_c("MA_", 5:10))
 
 
-
-
-
-# 5.グループごとにローリング計算 -----------------------------------------------------
+# 4 グループごとにローリング計算 -----------------------------------------------------
 
 # 定義
 # --- 移動平均
@@ -101,8 +103,7 @@ FANG %>%
   slice(1:5)
 
 
-
-# 6.複数引数を持つローリング計算 -----------------------------------------------------
+# 5 複数引数を持つローリング計算 -----------------------------------------------------
 
 # 定義
 # --- ローリング相関係数
@@ -115,24 +116,24 @@ FB %>%
 
 
 
-# 7.関数定義/ラムダ式で定義 -----------------------------------------------------
+# 6 関数定義/ラムダ式で定義 -----------------------------------------------------
 
+# 関数定義
+# --- 通常の定義
+avg_of_avgs <-
+  slidify(function(x, y, z) (mean(x) + mean(y) + mean(z)) / 3,
+          .period = 10,
+          .align = "right")
 
-avg_of_avgs <- slidify(
-    function(x, y, z) (mean(x) + mean(y) + mean(z)) / 3,
-    .period = 10,
-    .align = "right"
-)
+# 関数定義
+# --- ラムダ式
+avg_of_avgs <-
+  slidify(~(mean(..1) + mean(..2) + mean(..3)) / 3,
+          .period = 10,
+          .align  = "right")
 
-# Or
-avg_of_avgs <- slidify(
-    ~(mean(..1) + mean(..2) + mean(..3)) / 3,
-    .period = 10,
-    .align  = "right"
-)
-
-FB %>%
-    mutate(avg_of_avgs = avg_of_avgs(open, high, low))
+# 列の追加
+FB %>% mutate(avg_of_avgs = avg_of_avgs(open, high, low))
 
 
 
@@ -146,16 +147,17 @@ FB %>%
 
 
 
-# 7.ローリング回帰 -----------------------------------------------------
+# 7 ローリング回帰 -----------------------------------------------------
 
 # 関数定義
 lm_roll <- slidify(~lm(.x ~ .y), .period = 90, .unlist = FALSE, .align = "right")
 
+# ローリング回帰の追加
 FB %>%
-    drop_na() %>%
-    mutate(numeric_date = as.numeric(date)) %>%
-    mutate(rolling_lm = lm_roll(adjusted, numeric_date)) %>%
-    filter(!is.na(rolling_lm))
+  drop_na() %>%
+  mutate(numeric_date = as.numeric(date)) %>%
+  mutate(rolling_lm = lm_roll(adjusted, numeric_date)) %>%
+  filter(!is.na(rolling_lm))
 
 
 
